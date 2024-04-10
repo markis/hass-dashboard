84720143b112bb8eb6fbc026e2ca8afb585f4b40
Markis Taylor <m@rkis.net>
2024-04-09 21:23:56 -0400

    feat: generate image less frequently (#10)

--- dashboard/__main__.py
+++ dashboard/__main__.py
@@ -5,7 +5,7 @@ from dashboard.generate import generate_image
 
 async def generate_image_every_minute() -> None:
     while True:
-        await asyncio.gather(asyncio.sleep(60), generate_image())
+        await asyncio.gather(asyncio.sleep(600), generate_image())
 
 
 def run() -> None:
